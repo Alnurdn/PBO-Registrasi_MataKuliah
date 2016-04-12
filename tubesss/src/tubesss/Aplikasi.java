8c89b24a1ff663b1409d433deaa84834bfa86ae6
ANZYM <ANZYM@AKUINI-PC>
2016-04-12 14:12:05 +0700

    penambahan Aplikasi

--- tubesss/src/tubesss/Aplikasi.java
+++ tubesss/src/tubesss/Aplikasi.java
@@ -9,24 +9,43 @@ package tubesss;
  *
  * @author ANZYM
  */
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
 public class Aplikasi {
-    private Dosen[] daftarDosen;
-    private Mahasiswa[] daftarMahasiswa;
+    private ArrayList<Dosen> daftarDosen = new ArrayList<>();
+    private ArrayList<Mahasiswa> daftarMahasiswa = new ArrayList<>();
     private Kelas daftarKelas;
-    
+    private ArrayList<MataKuliah> daftarMataKuliah = new ArrayList<>();    
+    private int nDosen = 0;
+    final private int maxDosen = 100;
+    final private int maxMahasiswa = 300;
+    private int nMahasiswa = 0;
+ 
     public void addDosen (Dosen Dsn) {
-        
+        daftarDosen.add(Dsn);
     }
-    
+   
     public void addMahasiswa (Mahasiswa Mhs) {
-        
+           daftarMahasiswa.add(Mhs);
     }
     
-    public Mahasiswa getMahasiswa (Mahasiswa id) {
-        return id;
+    public Mahasiswa getMahasiswa (int id) {
+        return daftarMahasiswa.get(id);
     }
     
     public void deleteMahasiswa (int id) {
-        
+        daftarMahasiswa.remove(id);
     }    
+    
+    public Mahasiswa searchMahasiswa (int id) {
+        return daftarMahasiswa.get(id);
+    }
+    
+    public Dosen searchDosen (int id) {
+        return daftarDosen.get(id);
+    }
+
 }
